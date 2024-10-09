2880379e9895a5c6318a94c7fac71e7be4872675
MohdArman123 <110353885+MohdArman123@users.noreply.github.com>
2024-10-09 16:27:35 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -8,15 +8,16 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_108.0.5359.125-1_amd64.deb
-  dpkg -x ./google-chrome-stable_108.0.5359.125-1_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_108.0.5359.125-1_amd64.deb
+  # Use Chrome 108.0.5359.125 from the archive
+  wget -P ./ https://commondatastorage.googleapis.com/chromium-browser-official/chromium-108.0.5359.125.tar.xz
+  tar -xf ./chromium-108.0.5359.125.tar.xz --strip-components=1
+  rm ./chromium-108.0.5359.125.tar.xz
   cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
 fi
 
 # Add Chrome's location to the PATH
-export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+export PATH="${PATH}:/opt/render/project/.render/chrome"
 
 # Continue with your build commands...
