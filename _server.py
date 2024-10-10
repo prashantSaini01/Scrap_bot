1820b646a800aad0be0f23b96e84873ecf8d48de
Anjali Saxena <saxenaanjali1830@gmail.com>
2024-10-10 12:51:45 +0530

    Update _server.py

--- _server.py
+++ _server.py
@@ -388,6 +388,10 @@ def scrape_instagram():
     finally:
         driver.quit()  # Ensure to quit the driver properly
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))  # Fallback to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port)
+
  
