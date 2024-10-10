16460e9a78be372751f6951c06db5b1a6da417fe
Anjali Saxena <saxenaanjali1830@gmail.com>
2024-10-10 12:50:20 +0530

    Update twitter_scraper.py

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -115,9 +115,9 @@ def scrape_twitter(data):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-software-rasterizer")
     chrome_options.add_argument("--window-size=1920,1080")
+
 
     # Initialize WebDriver
     # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
